2b60d8a0b7112ba3cfd6413b7c8973c7ee70c68c
wichol0717 <wichol0717@gmail.com>
2026-06-25 01:12:17 -0600

    nueva paleta Majito

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          crema: "#FFF2E6",
+          sunset: "#F7D6C1",
+          pink: "#F48FB1",
+          orange: "#F15A3A",
+          mocha: "#CBA08A",
+        },
+      },
+    },
   },
   plugins: [],
 }
